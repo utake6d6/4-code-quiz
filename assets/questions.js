f4edc6c1a8bfefe7b4c554c48e95aa09c4304604
Bert <robertgill82@gmail.com>
2020-10-18 16:40:13 -0700

    css completed, pseudocode for questions.js

---
+++ assets/questions.js
@@ -0,0 +1,18 @@
+// Var with array and object for questions 
+
+// Declared variables
+
+// Start of working code 
+// Declared variables
+
+// Seconds left is 15 seconds per question:
+
+// Triggers timer on button, shows user a display on the screen
+
+// Renders questions and choices to page: 
+
+// Event to compare choices with answer
+
+// Append last page
+
+
